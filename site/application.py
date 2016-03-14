cad6a1183837ee9c10146d648de83c48c0abd32d
Alban Tiberghien <alban.tiberghien@gmail.com>
2016-03-14 07:47:32 +0100

    Add context variable

--- site/application.py
+++ site/application.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, send_from_directory
-
+import getpass
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    username = getpass.getuser()
+    return render_template('homepage.html',
+                           username=username,
+                           active_page='home')
 
 @app.route('/gallery')
 def gallery():
-    return render_template('gallery.html')
+    photos = []
+    for x in range(30):
+        photos.append({
+            "name" : "Chat %s" % (x+1),
+            "caption" : "Super Chat %s" % (x+1),
+            "url" : "http://lorempixel.com/300/200/cats/",
+        })
+    return render_template('gallery.html',
+                            photos=photos,
+                            active_page='gallery')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html',
+                            active_page='contact')
 
 @app.route('/bower_components/<path:path>')
 def send_bower_components(path):
